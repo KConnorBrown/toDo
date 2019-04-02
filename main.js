0d3117aeddf9f5aa872326dd499bba3d495e33c3
Connor Brown <kenneth_brown@brown.edu>
2019-04-01 23:17:41 -0400

    tracery starting to incorporate

--- main.js
+++ main.js
@@ -1,59 +1,46 @@
 $(document).ready(function() {
 
 	var jsonObj = {
-		"origin": [
-			"#NP# #VP#",
-			"#IN# #NP# #VP#"
-		],
-		"NP": [
-			"#Det# #N#",
-			"#Det# #Adj# #N#",
-			"#Det# #N# #PP#",
-			"#N#"
-		],
-		"PP": [
-			"#Prep# #NP#"
-		],
-		"Prep": [
-			"in",
-			"on",
-			"over",
-			"against",
-			"with"
-		],
-		"VP": [
-			"#Vtrans# #NP#",
-			"#Vintr#",
-			"hasOwnProperty",
-			"isFinite",
-			"isNaN",
-			"isPrototypeOf #NP#"
-		],
-		"Det": [
-			"this"
-		],
-		"N": ["Array","Date", "Math", "NaN", "Number", "Object", "String", "char","Infinity","function", "length", "name", "prototype", "class", "switch", "default",
-	        "boolean", "byte", "case", "const", "debugger", "int", "interface", "package", "return", "var", "void", "argument"],
-	"Vintr": ["continue", "float", "vocalize", "return"],
-		"Adj": ["protected", "synchronized", "double","default", "undefined", "true","false", "static", "super", "public", "native", "new", "abstract",
-	          "final", "long", "null", "private", "short", "transient","void","volatile"],
-		"Vtrans": ["catch", "do", "delete", "break", "continue", "extend", "implement", "let","throw","try","yield","eval",
-	             "export", "import", "return","goto","void"],
-	  "IN":["if", "for", "while", "with"],
-	  "RB":["finally","else"]
-	};
+	"origin": [
+		"#action# the #work# for #class#"
+	],
+	"class": [
+		"cs15",
+		"healthcare in the US",
+		"coding 4 language",
+		"logic 4 systems",
+		"cs1950y",
+		"how poems see",
+		"English"
+	],
+	"work": [
+		"homework",
+		"reading",
+		"p-set",
+		"essay",
+		"outline",
+		"presentation"
+	],
+	"action": [
+		"do",
+		"finish",
+		"start",
+		"turn in",
+		"review"
+	]
+}
 
 	var grammar = tracery.createGrammar(jsonObj);
 	var text = grammar.flatten('#origin#');
 	$('#1').html(text);
 
-  function loadJsonFromFile() {
-		// You can load directly from local files in firefox but not in Chrome
-		// To load from local files in chrome, check https://www.maketecheasier.com/setup-local-web-server-all-platforms/
-		$.getJSON("https://raw.githubusercontent.com/cqx931/Coding4Language/master/tutorials/week2/01_tracery_js/grammar_tracery.json", function(json) {
-	     console.log(json)
-	  });
-  };
-
-	loadJsonFromFile();
+  // function loadJsonFromFile() {
+	// 	// You can load directly from local files in firefox but not in Chrome
+	// 	// To load from local files in chrome, check https://www.maketecheasier.com/setup-local-web-server-all-platforms/
+	// 	$.getJSON("https://raw.githubusercontent.com/cqx931/Coding4Language/master/tutorials/week2/01_tracery_js/grammar_tracery.json", function(json) {
+	//      console.log(json)
+	//   });
+  // };
+  //
+	// loadJsonFromFile();
 });
